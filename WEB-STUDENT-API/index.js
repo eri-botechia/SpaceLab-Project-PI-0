f6d754d9c7457144c7832bd8e11b9fc43a1611d9
eri-Botechia <botechia94.erika@gmail.com>
2024-11-22 18:01:19 -0300

    vol-3-scaffolding and initial setup

---
+++ WEB-STUDENT-API/index.js
@@ -0,0 +1,22 @@
+const e = typeof {}
+const n = typeof 25
+const t = typeof "5"
+const r = typeof []
+const a = typeof true
+
+
+
+let nome = "Astrodev" && typeof t
+let verdadeiro = "ERI-BOTECHIA" && true 
+let presenca = true
+let idade = 28
+let fakeNews = "verdadeiro" && typeof boolean
+
+
+const arg2 = process.argv[2] 
+if( arg2 && arg2 == nome.toLowerCase(){
+    console.log("BEM VINDO ADMIN")
+}else{
+    console.log("ACESSO NEGADO")
+}
+
